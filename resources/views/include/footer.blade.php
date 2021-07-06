acb73904ab9167bbb418fa3d9b2f2cc2602d4307
Quint <quint.legius@iqfx.nl>
2021-07-06 19:47:23 +0200

    added base layout

---
+++ resources/views/include/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="text-white text-center text-lg-start fixed-bottom footer-sv">
+    <div class="text-center p-3">
+        Copyright &copy; <?php echo date("Y"); ?>&nbsp;
+      <a class="text-white" href="https://salvemundi.nl/" target="_blank">salvemundi.nl</a>
+    </div>
+</footer>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Links for css, etc. -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css"/>
+    <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    <title>{{ config('app.name', 'Laravel') }}</title>
+</head>
+<body id="body-pd">
+    @include('include/sidebar')
+    @yield('content')
+    @include('include/footer')
+</body>
+</html>

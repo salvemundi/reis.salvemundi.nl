c45791a2bfcde398e731ef284a51b00c9608a181
Albion <461340@student.fontys.nl>
2021-07-09 23:48:26 +0200

    Removed magicnumbers

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Participant;
+use App\Enums\Roles;
 
 class DashboardController extends Controller
 {
@@ -11,15 +12,15 @@ class DashboardController extends Controller
         $viewVars = [];
 
         $viewVars['amountTotal']                = Participant::count();
-        $viewVars['amountTotalCheckedIn']       = Participant::where('checkedIn', 1)->count();
-        $viewVars['amountCrew']                 = Participant::where('role', 3)->count();
-        $viewVars['amountCrewCheckedIn']        = Participant::where('role', 3)->where('checkedIn', 1)->count();
-        $viewVars['amountChildren']             = Participant::where('role', 0)->count();
-        $viewVars['amountChildrenCheckedIn']    = Participant::where('role', 0)->where('checkedIn', 1)->count();
-        $viewVars['amountParents']              = Participant::where('role', 1)->count();
-        $viewVars['amountParentsCheckedIn']     = Participant::where('role', 1)->where('checkedIn', 1)->count();
-        $viewVars['amountTeachers']             = Participant::where('role', 2)->count();
-        $viewVars['amountTeachersCheckedIn']    = Participant::where('role', 2)->where('checkedIn', 1)->count();
+        $viewVars['amountTotalCheckedIn']       = Participant::where('checkedIn', true)->count();
+        $viewVars['amountCrew']                 = Participant::where('role', Roles::crew)->count();
+        $viewVars['amountCrewCheckedIn']        = Participant::where('role', Roles::crew)->where('checkedIn', true)->count();
+        $viewVars['amountChildren']             = Participant::where('role', Roles::child)->count();
+        $viewVars['amountChildrenCheckedIn']    = Participant::where('role', Roles::child)->where('checkedIn', true)->count();
+        $viewVars['amountParents']              = Participant::where('role', Roles::dad_mom)->count();
+        $viewVars['amountParentsCheckedIn']     = Participant::where('role', Roles::dad_mom)->where('checkedIn', true)->count();
+        $viewVars['amountTeachers']             = Participant::where('role', Roles::teacher)->count();
+        $viewVars['amountTeachersCheckedIn']    = Participant::where('role', Roles::teacher)->where('checkedIn', true)->count();
 
         return view("dashboard", $viewVars);
     }

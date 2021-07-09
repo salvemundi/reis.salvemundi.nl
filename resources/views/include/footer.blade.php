b5ae2fbb7c5ca4bd232764830fa8080a2ec0690e
BryanAafjes <432731@student.fontys.nl>
2021-07-09 16:59:09 +0200

    made footer smaller, and fixed scrolling issue

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -1,5 +1,8 @@
+<div class="p-2">
+  &nbsp;
+</div>
 <footer class="text-white text-center text-lg-start fixed-bottom footer-sv">
-    <div class="text-center p-3">
+    <div class="text-center p-2">
         Copyright &copy; <?php echo date("Y"); ?>&nbsp;
       <a class="text-white" href="https://salvemundi.nl/" target="_blank">salvemundi.nl</a>
     </div>

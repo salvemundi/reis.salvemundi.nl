000ee32e418e17d53d0a9624b6b292a66bcda81f
Sanne <sanne2610h@gmail.com>
2021-07-10 14:08:46 +0200

    changed enums

--- app/Enums/Roles.php
+++ app/Enums/Roles.php
@@ -15,4 +15,22 @@ final class Roles extends Enum
     const dad_mom = 1;
     const teacher = 2;
     const crew = 3;
+
+    public static function getDescription($value): string
+    {
+        if ($value === self::child) {
+            return 'Kiddo';
+        }
+        if ($value === self::dad_mom) {
+            return 'Ouder / Begeleiding';
+        }
+        if ($value === self::teacher) {
+            return 'Leraar';
+        }
+        if ($value === self::crew) {
+            return 'Crew';
+        }
+    
+        return parent::getDescription($value);
+    }
 }

96cb583f2356b93574c30a3376eed5e520b15e92
Quint <quint.legius@iqfx.nl>
2021-08-19 19:40:06 +0200

    testing object conversion

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -15,10 +15,48 @@ class APIController extends Controller
         $url = "https://localhost/api/participants";
         $res = $client->get($url);
         $content = json_decode($res->getBody()->getContents());
-        //dd($content);
+        $arr = []
         foreach($content as $item)
         {
-            echo($item['firstName']);
+            // Try to find the participant first, then update their records instead of creating a new object
+            $tryToFind = Participant::where('samuId',$item->id)->first();
+            if($tryToFind)
+            {
+                $tryToFind->firstName = $item->firstName;
+                $tryToFind->insertion = $item->insertion;
+                $tryToFind->lastName = $item->lastName;
+                $tryToFind->birthday = strtotime($item->birthday);
+                $tryToFind->email = $item->email;
+                $tryToFind->phoneNumber = $item->phoneNumber;
+                $tryToFind->firstNameParent = $item->firstNameParent;
+                $tryToFind->lastNameParent = $item->lastNameParent;
+                $tryToFind->addressParent = $item->addressParent;
+                $tryToFind->medicalIssues = $item->medicalIssues;
+                $tryToFind->specials = $item->specials;
+                $tryToFind->phoneNumberParent = $item->phoneNumberParent;
+                $tryToFind->studentYear = $item->studentYear;
+                $arr->push($tryToFind);
+                //$tryToFind->save();
+            }
+
+            $newParticipant = new Participant();
+            $newParticipant->firstName = $item->firstName;
+            $newParticipant->insertion = $item->insertion;
+            $newParticipant->lastName = $item->lastName;
+            $newParticipant->birthday = strtotime($item->birthday);
+            $newParticipant->email = $item->email;
+            $newParticipant->phoneNumber = $item->phoneNumber;
+            $newParticipant->firstNameParent = $item->firstNameParent;
+            $newParticipant->lastNameParent = $item->lastNameParent;
+            $newParticipant->addressParent = $item->addressParent;
+            $newParticipant->medicalIssues = $item->medicalIssues;
+            $newParticipant->specials = $item->specials;
+            $newParticipant->phoneNumberParent = $item->phoneNumberParent;
+            $newParticipant->studentYear = $item->studentYear;
+            $newParticipant->samuId = $item->id;
+            $arr->push($tryToFind);
+            //$newParticipant->save();
         }
+        dd($arr);
     }
 }

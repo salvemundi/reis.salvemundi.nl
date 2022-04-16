b4b7157a81e5fe705dfc2c376f4e3a6a0b7649fb
Albion <461340@student.fontys.nl>
2022-04-16 16:53:13 +0200

    added pie

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -1,8 +1,40 @@
 @extends('layouts.guapp')
 @section('content')
+<div>
+    <canvas id="myChart"></canvas>
+</div>
 @foreach($posts as $post)
     <p>
         {{ $post->name }}
     </p>
 @endforeach
+
+<script>
+    const data = {
+        labels: [
+            'Bezet',
+            'Nog vrij',
+        ],
+        datasets: [{
+            label: 'Aantal plekken vrij!',
+            data: [300, 159],
+            backgroundColor: [
+                'rgb(255, 99, 132)',
+                'rgb(54, 162, 235)',
+            ],
+            hoverOffset: 4
+        }]
+
+    };
+    const config = {
+        type: 'doughnut',
+        data: data,
+    };
+
+    const myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+    );
+</script>
+
 @endsection

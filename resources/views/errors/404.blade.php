c44ffac0e70c6ad35c5d2e2c5eda2cb32672035c
Quint <quint.legius@gmail.com>
2022-05-10 10:55:17 +0200

    updated error views

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.guapp')
 @section('content')
     <div class="row tex-center">
-        <h1>403</h1>
+        <h1 class="center mt-5">404</h1>
         <p>Pagina niet gevonden.</p>
     </div>
 @endsection

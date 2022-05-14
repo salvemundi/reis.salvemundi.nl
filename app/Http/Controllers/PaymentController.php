03068dc7228a517cd91592f2f9e019f4b5f4f678
Albion <461340@student.fontys.nl>
2022-05-14 14:34:42 +0200

    MORE FIXES

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -22,7 +22,7 @@ class PaymentController extends Controller
                     "value" => "90.00"
                 ],
                 "description" => "Introductie ". Date("Y"),
-                "redirectUrl" => route('payment.success'),
+                "redirectUrl" => route('payment.success', ['confirmationID' => $confirmationToken->id]),
                 "webhookUrl"  => route('webhooks.mollie'),
                 "metadata" => [
                     "payment_id" => $paymentObject->id,

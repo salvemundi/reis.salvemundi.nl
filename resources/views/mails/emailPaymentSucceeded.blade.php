20c29ebfdcca83f2c48379a3360c29ef16a6a0aa
Albion <461340@student.fontys.nl>
2022-07-15 16:43:43 +0200

    added blogs link to mail

--- resources/views/mails/emailPaymentSucceeded.blade.php
+++ resources/views/mails/emailPaymentSucceeded.blade.php
@@ -7,6 +7,8 @@ Bedankt voor het inschrijven voor de intro!
 
 Bewaar de QR-code in de bijlagen goed!
 
+Voor meer nieuws en updates: https://intro.salvemundi.nl/blogs
+
 Met vriendelijke groet,
 
 Salve Mundi <br>

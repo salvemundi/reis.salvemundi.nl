a49882efc20b976fdfff112725e0fbb14517c6f1
Quint <quint.legius@iqfx.nl>
2022-07-17 19:14:45 +0200

    Fixed huge problem

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -20,7 +20,7 @@ class DashboardController extends Controller
         $viewVars['amountCrew']                 = Participant::where('role', Roles::crew)->count();
         $viewVars['amountCrewCheckedIn']        = Participant::where('role', Roles::crew)->where('checkedIn', true)->count();
         $viewVars['amountChildren']             = Participant::where('role', Roles::child)->count();
-        $viewVars['amountChildrenPaid']             = count($this->paymentController->getAllPaidUsers());
+        $viewVars['amountChildrenPaid']         = count($this->paymentController->getAllPaidUsers());
         $viewVars['amountChildrenCheckedIn']    = Participant::where('role', Roles::child)->where('checkedIn', true)->count();
         $viewVars['amountParents']              = Participant::where('role', Roles::dad_mom)->count();
         $viewVars['amountParentsCheckedIn']     = Participant::where('role', Roles::dad_mom)->where('checkedIn', true)->count();

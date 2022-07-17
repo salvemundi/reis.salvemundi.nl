96a1dc28233b624c269fb035aaacba521ecf9130
Quint <quint.legius@gmail.com>
2022-07-17 03:53:39 +0200

    removed commented code

--- resources/views/admin/qr.blade.php
+++ resources/views/admin/qr.blade.php
@@ -7,7 +7,6 @@
 <div class="row">
     <div>
         <main class="wrapper">
-
             <section class="center" style="display: flex; margin-top: 2em; text-align: center" id="demo-content">
                 <div>
                     <h1 class="title">Scan QR Code om in / uit the checken!</h1>
@@ -31,18 +30,7 @@
                     <div>
                         <video id="video" width="300" height="200" style="border: 1px solid gray"></video>
                     </div>
-
                 </div>
-{{--                <svg id="greencheck" class="responses" style="display: none" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2">--}}
-{{--                    <circle class="path circle" fill="none" stroke="#73AF55" stroke-width="6" stroke-miterlimit="10" cx="65.1" cy="65.1" r="62.1"/>--}}
-{{--                    <polyline class="path check" fill="none" stroke="#73AF55" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" points="100.2,40.2 51.5,88.8 29.8,67.5 "/>--}}
-{{--                </svg>--}}
-
-{{--                <svg id="redcheck" style="display:none;" class="responses" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2">--}}
-{{--                    <circle class="path circle" fill="none" stroke="#D06079" stroke-width="6" stroke-miterlimit="10" cx="65.1" cy="65.1" r="62.1"/>--}}
-{{--                    <line class="path line" fill="none" stroke="#D06079" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" x1="34.4" y1="37.9" x2="95.8" y2="92.3"/>--}}
-{{--                    <line class="path line" fill="none" stroke="#D06079" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" x1="95.8" y1="38" x2="34.4" y2="92.2"/>--}}
-{{--                </svg>--}}
             </section>
         </main>
     </div>

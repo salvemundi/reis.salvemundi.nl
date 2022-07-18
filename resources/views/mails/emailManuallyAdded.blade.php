f7e330c2a70aa7d275abea45bcbde74ce1101f9f
Albion <461340@student.fontys.nl>
2022-07-19 00:42:55 +0200

    Testing for iqfx

---
+++ resources/views/mails/emailManuallyAdded.blade.php
@@ -0,0 +1,14 @@
+@component('mail::message')
+
+Beste {{ $participant->firstName }},
+
+Bewaar de QR-code in de bijlagen goed, deze heb je nodig tijdens de introductie!
+
+Voor meer nieuws en updates: https://intro.salvemundi.nl/blogs
+
+Met vriendelijke groet,
+
+Salve Mundi <br>
+Rachelsmolen 1 <br>
+5612 MA Eindhoven
+@endcomponent

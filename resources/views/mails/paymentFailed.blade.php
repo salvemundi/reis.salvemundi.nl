2be4a9cdccca7db8791a37ef3be307c36950cb9e
Albion <461340@student.fontys.nl>
2022-07-21 22:39:54 +0200

    lil text change

--- resources/views/mails/paymentFailed.blade.php
+++ resources/views/mails/paymentFailed.blade.php
@@ -7,8 +7,7 @@ Probeer het later opnieuw!
 
 Mocht het je meerdere keren niet lukken of is het geld wel afgeschreven maar ontvang je alsnog deze mail?
 
-Neem dan contact op met per mail: info@salvemundi.nl
-Of via WhatsApp: +31 6 24827777
+Neem dan contact op met per mail: info@salvemundi.nl of via WhatsApp: <a href="https://wa.me/+31624827777" target="_blank">+31 6 24827777</a>
 
 Jouw betalings ID: {{ $payment->mollie_transaction_id }}
 

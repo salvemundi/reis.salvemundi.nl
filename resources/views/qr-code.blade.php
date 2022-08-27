31a060a18b38c2f8890121131a015bd0d8e8bd7f
Albion <461340@student.fontys.nl>
2022-08-27 20:01:30 +0200

    yeet

--- resources/views/qr-code.blade.php
+++ resources/views/qr-code.blade.php
@@ -87,12 +87,10 @@
             <div class="col-md-6 col-6">
                 <img class="imgQR" src="images/vertrouwensPersonen/femke.jpeg">
                 <b><p class="vertrouwensPersoonText">Femke</p></b>
-                <a class="btn btn-primary mt-1" href="tel:+31 6 36000643"><i class="fa fa-phone"></i> +31 6 36000643</a>
             </div>
             <div class="col-md-6 col-6">
                 <img class="imgQR" src="images/vertrouwensPersonen/jimmy.jpg">
                 <b><p class="vertrouwensPersoonText">Jimmy</p></b>
-                <a class="btn btn-primary mt-1" href="tel:+31 6 51955040"><i class="fa fa-phone"></i> +31 6 51955040</a>
             </div>
         </div>
     </div>

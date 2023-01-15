290d7edeb8d212ea5ee1cbe80e8a10b6dc5e9014
Quint Legius <quint.legius@gmail.com>
2023-01-15 16:31:28 +0100

    fixed email sending qrcodes

--- app/Mail/parentMailSignup.php
+++ app/Mail/parentMailSignup.php
@@ -33,7 +33,7 @@ class parentMailSignup extends Mailable
     public function build()
     {
         return $this->markdown('mails/parentSignup', ['participant' => $this->participant])->subject('Intro 2022 bevestiging')
-            ->attachData(Image::canvas(290,290,"#fff")->insert(base64_decode(DNS2D::getBarcodePNG($this->participant->id, 'QRCODE', 10,10)))->resizeCanvas(20*2, 20*2, 'center', true, "#fff")->encode('jpg'),'qrcode.jpg', [
+            ->attachData((string)Image::canvas(290,290,"#fff")->insert(base64_decode(DNS2D::getBarcodePNG($this->participant->id, 'QRCODE', 10,10)))->resizeCanvas(20*2, 20*2, 'center', true, "#fff")->encode('jpg'),'qrcode.jpg', [
                 'as' => 'qrcode.jpg',
                 'mime' => 'application/jpg',
             ]);

a9cce88a16b1126b0e8e3d9f53dd4276284db6b4
Quint Legius <quint.legius@gmail.com>
2023-04-02 18:54:09 +0200

    fixed dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Participant;
 use App\Enums\Roles;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 
 class DashboardController extends Controller
 {
@@ -13,13 +16,15 @@ class DashboardController extends Controller
         $this->paymentController = new PaymentController();
     }
 
-    public function index() {
+    public function index(): Factory|View|Application
+    {
         $viewVars = [];
 
         $viewVars['amountTotalCheckedIn']           = Participant::where('checkedIn', true)->count();
         $viewVars['amountCrew']                     = Participant::where('role', Roles::crew)->count();
         $viewVars['amountCrewCheckedIn']            = Participant::where('role', Roles::crew)->where('checkedIn', true)->count();
         $viewVars['amountParticipants']             = Participant::where('role', Roles::participant)->where('checkedIn', true)->count();
+        $viewVars['amountAllTravelers']             = Participant::all()->count();
         $viewVars['amountParticipantsPaid']         = count($this->paymentController->getAllPaidUsers());
         $viewVars['amountParticipantsCheckedIn']    = Participant::where('role', Roles::participant)->where('checkedIn', true)->count();
         $viewVars['amountEveryone']                 = $viewVars['amountParticipantsPaid'] + $viewVars['amountCrew'];

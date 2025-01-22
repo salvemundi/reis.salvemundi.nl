a8825a7be24179d780988961436635ed936368d1
Quint Legius <quint.legius@gmail.com>
2025-01-22 21:17:19 +0100

    quick fox

--- resources/views/admin/addActivities.blade.php
+++ resources/views/admin/addActivities.blade.php
@@ -54,33 +54,35 @@
                     <input class="btn btn-primary" type="submit" value="Opslaan">
                 </div>
             </form>
-            <h5>Inschrijvingen</h5>
-            <nav>
-                <div class="nav nav-tabs d-flex flex-row justify-content-start" id="nav-tab" role="tablist">
-                    <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Wel ingeschreven</button>
-                    <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Niet ingeschreven</button>
-                </div>
-            </nav>
-            <div class="tab-content" id="myTabContent">
-                <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
-                    @foreach($activity->participants as $participant)
-                        <div class="card">
-                            <div class="card-body text-black">
-                                {{$participant->getFullName()}}
+            @if(isset($activity))
+                <h5>Inschrijvingen</h5>
+                <nav>
+                    <div class="nav nav-tabs d-flex flex-row justify-content-start" id="nav-tab" role="tablist">
+                        <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Wel ingeschreven</button>
+                        <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Niet ingeschreven</button>
+                    </div>
+                </nav>
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="home-tab" tabindex="0">
+                        @foreach($activity->participants as $participant)
+                            <div class="card">
+                                <div class="card-body text-black">
+                                    {{$participant->getFullName()}}
+                                </div>
                             </div>
-                        </div>
-                    @endforeach
-                </div>
-                <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
-                    @foreach($unlinkedParticipants as $participant)
-                        <div class="card">
-                            <div class="card-body text-black">
-                                {{$participant->getFullName()}}
+                        @endforeach
+                    </div>
+                    <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">
+                        @foreach($unlinkedParticipants as $participant)
+                            <div class="card">
+                                <div class="card-body text-black">
+                                    {{$participant->getFullName()}}
+                                </div>
                             </div>
-                        </div>
-                    @endforeach
+                        @endforeach
+                    </div>
                 </div>
-            </div>
+            @endif
         </div>
     </div>
 @endsection
